01967f93955dad46190ce74637fdefd97d42c88b
Yanbo Guo <amberguo1992@gmail.com>
2017-12-05 17:16:34 -0500

    Add files via upload

--- OnlineShopping/account.java
+++ OnlineShopping/account.java
@@ -9,6 +9,7 @@ public class account {
 	public String password = null;
 	public Profile profile = null;
 	public shoppingcart SC = null;
+	public CardInfo cardInfo = null;
 
 	public account(String username, String password) {
 		this.username = username;
@@ -26,54 +27,6 @@ public class account {
 		return answer;
 	}
 
-	public void list() {
-		System.out.println("Please select a number");
-		System.out.println("1: Profile");
-		System.out.println("2: Order");
-		System.out.println("3: Shopping Cart");
-		System.out.println("4: Payment Method");
-		System.out.println("5: Home");
-		System.out.println("6: Sign out");
-		if (accountscanner.hasNextInt()) {
-			int Number = accountscanner.nextInt();
-			accountscanner.nextLine();
-			if (Number == 1) {
-				this.profile.list();
-			} 
-			else if (Number == 2) {
-			} 
-			else if (Number == 3){
-				this.SC.list();
-			}
-			else if (Number == 5) {
-				shoppingSystem.web.list();
-			}
-			
 
-			else if (Number == 6) {
-				this.Signout();
-			}
 
-			else {
-
-				System.out.println("Please enter the right number");
-				this.list();
-
-			}
-		}
-
-		else {
-			accountscanner.nextLine();
-			System.out.println("Please enter the right number");
-			this.list();
-		}
-
-	}
-
-	public void Signout() {
-		System.out.println("Thank you for choosing us!");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("----------------------------------------------");
-		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	}
 }
